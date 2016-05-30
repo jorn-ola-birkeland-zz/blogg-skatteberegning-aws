1c4272a1185d51c77cc4c0eac7891bedaa0c62a4
Jørn Ola Birkeland <jornola.birkeland@gmail.com>
2016-05-30 11:43:06 +0200

    Update SkatteberegningKinesis.java

--- SkatteberegningKinesis.java
+++ SkatteberegningKinesis.java
@@ -5,7 +5,7 @@ import com.amazonaws.services.lambda.runtime.events.KinesisEvent;
 public class SkatteberegningKinesis {
 
     public void recordHandler(KinesisEvent event) {
-        System.out.println("Kinesis-hendelse mottatt. # records " + event.getRecords().size());
+        System.out.println("Kinesis-hendelse mottatt. # dataelementer " + event.getRecords().size());
     }
 
 }
